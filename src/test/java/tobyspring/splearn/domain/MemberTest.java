b01de1a7171c4197741745b71ba99da6cd6c6ffa
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-05 13:37:47 +0900

    test: Member 엔티티 생성 테스트 코드 작성

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package tobyspring.splearn.domain;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemberTest {
-  
+
+    @Test
+    void createMember() {
+        var member = new Member("jypark@splearn.app", "Jiyong", "secret");
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
 }
