bf16194e015f0e8eb03083f6a9db094349fab145
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-07 18:57:37 +0900

    test: constructorNullCheck, activate 테스트 코드 추가

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -3,14 +3,27 @@ package tobyspring.splearn.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberTest {
-
     @Test
     void createMember() {
         var member = new Member("jypark@splearn.app", "Jiyong", "secret");
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+    
+    @Test
+    void constructorNullCheck() {
+        assertThatThrownBy(() -> new Member(null, "Jiyong", "secret"))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("jypark@splearn.app", "Jiyong", "secret");
+        member.activate();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
     }
 }
