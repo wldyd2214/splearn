064109c6152cb5974be9dbe100a3aeacf35df363
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-13 13:33:33 +0900

    test: Member 엔티티 자연키 설정 테스트 코드 작성

--- src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -4,9 +4,11 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static tobyspring.splearn.domain.MemberFixture.*;
 
 @DataJpaTest
@@ -28,5 +30,15 @@ class MemberRepositoryTest {
       assertThat(member.getId()).isNotNull();
 
       entityManager.flush();
+    }
+    
+    @Test
+    void duplicateEmailFail() {
+        Member member = Member.register(createMemberRegisterCommand(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        Member member2 = Member.register(createMemberRegisterCommand(), createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+            .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
