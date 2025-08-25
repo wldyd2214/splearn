a52c37646e1b6cb9b4cbeb0c66cc6b200492b3cb
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-25 18:52:59 +0900

    faet: 표준 유효성 검사 도구를 이용한 요청 데이터 검증

--- src/main/java/tobyspring/splearn/application/provided/MemberRegister.java
+++ src/main/java/tobyspring/splearn/application/provided/MemberRegister.java
@@ -1,5 +1,6 @@
 package tobyspring.splearn.application.provided;
 
+import jakarta.validation.Valid;
 import tobyspring.splearn.domain.Member;
 import tobyspring.splearn.domain.MemberRegisterCommand;
 
@@ -7,5 +8,5 @@ import tobyspring.splearn.domain.MemberRegisterCommand;
  * 회원의 등록과 관련된 기능을 제공한다
  */
 public interface MemberRegister {
-    Member register(MemberRegisterCommand registerCommand);
+    Member register(@Valid MemberRegisterCommand registerCommand);
 }
