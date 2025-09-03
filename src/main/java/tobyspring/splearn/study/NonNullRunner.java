1b547b09f5923706d0191115867e043d04bc9d3d
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-09-03 19:17:55 +0900

    feat: NonNullRunner 소스 주석 처리

--- src/main/java/tobyspring/splearn/study/NonNullRunner.java
+++ src/main/java/tobyspring/splearn/study/NonNullRunner.java
@@ -3,12 +3,12 @@ package tobyspring.splearn.study;
 import org.springframework.lang.NonNull;
 
 public class NonNullRunner {
-    public static void main(String[] args) {
-        String name = null;
-        print(name);
-    }
-
-    static void print(@NonNull String name) {
-        System.out.println(name);
-    }
+//    public static void main(String[] args) {
+//        String name = null;
+//        print(name);
+//    }
+//
+//    static void print(@NonNull String name) {
+//        System.out.println(name);
+//    }
 }
