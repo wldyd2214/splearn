325c162b4b89e62fc14ed409bf64c2d4c275d80a
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-09-14 19:43:20 +0900

    test: SecurePasswordEncoderTest 코드 작성

--- src/test/java/tobyspring/splearn/adapter/security/SecurePasswordEncoderTest.java
+++ src/test/java/tobyspring/splearn/adapter/security/SecurePasswordEncoderTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package tobyspring.splearn.adapter.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 class SecurePasswordEncoderTest {
-  
+    @Test
+    void securePasswordEncore() {
+        SecurePasswordEncoder securePasswordEncoder = new SecurePasswordEncoder();
+
+        String passwordHash = securePasswordEncoder.encode("secret");
+
+        assertThat(securePasswordEncoder.matches("secret", passwordHash)).isTrue();
+        assertThat(securePasswordEncoder.matches("wrong", passwordHash)).isFalse();
+    }
+
 }
